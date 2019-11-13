f9fd5ca06cbeed0d4cd16024a4c6f6f6e98739ff
Aleksandr Andreev <alivemenone@gmail.com>
2019-11-13 17:15:49 +0300

    add models

---
+++ VirtoCommerce.WebhooksModule.Data/Models/WebhookEntity.cs
@@ -0,0 +1,16 @@
+﻿using VirtoCommerce.Platform.Core.Common;
+
+namespace VirtoCommerce.WebhooksModule.Data.Models
+{
+    public class WebhookEntity : AuditableEntity
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string ContentType { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsAllEvents { get; set; }
+        public long RaisedEventCount { get; set; }
+        public WebhookEvent[] Events { get; set; }
+
+    }
+}
