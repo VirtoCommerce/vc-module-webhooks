eab1ef620362dd4c823484705ebf616ddd9d043f
alexandr andreev <alivemenone@gmail.com>
2019-11-14 17:50:16 +0300

    Vp-910: Add repository; (#7)
    * add models
    * add repository
    * add repository
    update webHook service
    connect UI to API

--- VirtoCommerce.WebhooksModule.Core/Models/WebhookSearchCriteria.cs
+++ VirtoCommerce.WebhooksModule.Core/Models/WebhookSearchCriteria.cs
@@ -1,9 +1,10 @@
-﻿using VirtoCommerce.Domain.Commerce.Model.Search;
+﻿using System;
+using VirtoCommerce.Domain.Commerce.Model.Search;
 
 namespace VirtoCommerce.WebHooksModule.Core.Models
 {
-	public class WebHookSearchCriteria : SearchCriteriaBase
-	{
-		public bool IsActive { get; set; }
-	}
+    public class WebHookSearchCriteria : SearchCriteriaBase
+    {
+        public Nullable<bool> IsActive { get; set; }
+    }
 }
--- VirtoCommerce.WebhooksModule.Web/Scripts/blades/webhooks-list.js
+++ VirtoCommerce.WebhooksModule.Web/Scripts/blades/webhooks-list.js
@@ -1,5 +1,5 @@
 ﻿angular.module('virtoCommerce.webhooksModule')
-    .controller('virtoCommerce.webhooksModule.webhooksListController', ['$scope', 'virtoCommerce.webhooksModule.webApi', 'platformWebApp.bladeNavigationService', 'platformWebApp.dialogService', 'platformWebApp.authService', 'platformWebApp.uiGridHelper', 'platformWebApp.bladeUtils', '$timeout', function ($scope, webhookApi, bladeNavigationService, dialogService, authService, uiGridHelper, bladeUtils, $timeout) {
+    .controller('virtoCommerce.webhooksModule.webhooksListController', ['$scope', 'virtoCommerce.webhooksModule.webApi', 'platformWebApp.bladeNavigationService', 'platformWebApp.dialogService', 'platformWebApp.authService', 'platformWebApp.uiGridHelper', 'platformWebApp.bladeUtils', '$timeout', function ($scope, webHookApi, bladeNavigationService, dialogService, authService, uiGridHelper, bladeUtils, $timeout) {
         $scope.uiGridConstants = uiGridHelper.uiGridConstants;
         var blade = $scope.blade;
         var selectedNode = null;
@@ -16,7 +16,7 @@
 
             var searchCriteria = getSearchCriteria();
 
-            webhookApi.search(searchCriteria,
+            webHookApi.search(searchCriteria,
                 function (data) {
                     $scope.items = data.results;
                     $scope.pageSettings.totalItems = $scope.items.length;
@@ -44,7 +44,7 @@
                 blade.isLoading = true;
                 var searchCriteria = getSearchCriteria();
 
-                webhookApi.search(searchCriteria,
+                webHookApi.search(searchCriteria,
                     function (data) {
                         $scope.items = $scope.items.concat(data.results);
                         $scope.pageSettings.totalItems = $scope.items.length;
@@ -61,25 +61,36 @@
                     }).$promise.finally(function () {
                     blade.isLoading = false;
                 });
-            };
+            }
         }
 
-
-
-        $scope.editWebhook = function(item) {
-
-        };
-
-        $scope.deleteWebhook = function(item) {
-
-        };
+        function deleteWebHooks(selection) {
+            var dialog = {
+                id: "confirmDelete",
+                title: "webhooks.dialogs.setting-delete.title",
+                message: "webhooks.dialogs.setting-delete.message",
+                callback: function(remove) {
+                    if (remove) {
+                        blade.isLoading = true;
+                        bladeNavigationService.closeChildrenBlades(blade);
+                        var ids = _.map(selection, function(item) { return item.id; });
+                        webHookApi.delete(ids,
+                            function() {
+                                blade.refresh();
+                            });
+                    }
+                }
+            };
+            dialogService.showConfirmationDialog(dialog);
+        }
 
 
         blade.setSelectedItem = function (listItem) {
             $scope.selectedNodeId = listItem.id;
         };
 
-        $scope.selectNode = function (e, listItem) {
+
+        $scope.selectItem = function (e, listItem) {
             blade.setSelectedItem(listItem);
             var newBlade = {
                 id: "webhookDetail",
@@ -91,6 +102,48 @@
             };
             bladeNavigationService.showBlade(newBlade, blade);
         };
+
+        $scope.selectNode = function (node, isNew) {
+            $scope.selectedNodeId = node.id;
+
+            var newBlade = {
+                id: 'webhookDetail',
+                controller: 'virtoCommerce.webhooksModule.webhookDetailController',
+                template: 'Modules/$(virtoCommerce.webhooksModule)/Scripts/blades/webhook-detail.tpl.html',
+                subtitle: 'webhooks.blades.webhook-detail.subtitle'
+            };
+
+            if (isNew) {
+                angular.extend(newBlade, {
+                    title: 'webhooks.blades.webhook-detail.title',
+                    
+                    isNew: true,
+                    saveCallback: function (newPricelist) {
+                        newBlade.isNew = false;
+                        blade.refresh(true).then(function () {
+                            newBlade.currentEntityId = newPricelist.id;
+                            bladeNavigationService.showBlade(newBlade, blade);
+                        });
+                    }
+                });
+            } else {
+                angular.extend(newBlade, {
+                    currentEntityId: node.id,
+                    title: node.name
+                });
+            }
+
+            bladeNavigationService.showBlade(newBlade, blade);
+        };
+
+
+        function isItemsChecked() {
+            return $scope.gridApi && _.any($scope.gridApi.selection.getSelectedRows());
+        }
+
+        function getSelectedItems() {
+            return $scope.gridApi.selection.getSelectedRows();
+        }
 
 
         blade.toolbarCommands = [
@@ -105,7 +158,9 @@
             {
                 name: "platform.commands.add",
                 icon: 'fa fa-plus',
-                executeMethod: blade.refresh,
+                executeMethod: function () {
+                    $scope.selectNode({}, true);
+                },
                 canExecuteMethod: function () {
                     return true;
                 }
@@ -113,10 +168,10 @@
             {
                 name: "platform.commands.remove",
                 icon: 'fa fa-trash',
-                executeMethod: blade.refresh,
-                canExecuteMethod: function () {
-                    return true;
-                }
+                executeMethod: function () {
+                    deleteWebHooks(getSelectedItems());
+                },
+                canExecuteMethod: isItemsChecked
             }
         ];
 
