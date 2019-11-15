77c4f5d9ba8120660cdc933d54dffdf51a259521
Andrey Kozlov <yecli.github@gmail.com>
2019-11-15 17:58:52 +0500

    VP-912: Subscription to all events
    - Added EventIds to WebHookSearchCriteria and supported it in search service;
    - Subscribed to all registered events. Still need to move all logic into background job and handle webhooks by batches;
    - Registered RegisteredEventStore as singleton to avoid discovering events each time;

--- VirtoCommerce.WebHooksModule.Data/Services/WebHookManager.cs
+++ VirtoCommerce.WebHooksModule.Data/Services/WebHookManager.cs
@@ -1,8 +1,12 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using VirtoCommerce.Platform.Core.Bus;
+using VirtoCommerce.Platform.Core.Events;
 using VirtoCommerce.WebHooksModule.Core.Models;
 using VirtoCommerce.WebHooksModule.Core.Services;
 
@@ -12,68 +16,91 @@ namespace VirtoCommerce.WebHooksModule.Data.Services
 	{
 		private readonly IRegisteredEventStore _registeredEventStore;
 		private readonly IHandlerRegistrar _eventHandlerRegistrar;
+		private readonly IWebHookSearchService _webHookSearchService;
 
-		public WebHookManager(IRegisteredEventStore registeredEventStore, IHandlerRegistrar eventHandlerRegistrar)
+		public WebHookManager(IRegisteredEventStore registeredEventStore,
+			IHandlerRegistrar eventHandlerRegistrar,
+			IWebHookSearchService webHookSearchService)
 		{
 			_registeredEventStore = registeredEventStore;
 			_eventHandlerRegistrar = eventHandlerRegistrar;
+			_webHookSearchService = webHookSearchService;
 		}
 
+		/// <inheritdoc />
 		public void SubscribeToAllEvents()
 		{
 			var allRegisteredEvents = _registeredEventStore.GetAllEvents();
 
-			//foreach (var registeredEvent in allRegisteredEvents)
-			//{
-			//	var eventType = registeredEvent.EventType;
-			//	var genericRegisterHandlerMethod = _eventHandlerRegistrar.GetType()
-			//		.GetMethods()
-			//		.FirstOrDefault(x => x.Name.EqualsInvariant("RegisterHandler") && x.GetParameters().Length == 1)
-			//		.MakeGenericMethod(eventType);
-
-			//	var getHandlderFuncGenericMethod = GetType()
-			//		.GetMethods()
-			//		.FirstOrDefault(x => x.Name.EqualsInvariant("GetHandlderFunc") && x.GetParameters().Length == 1)
-			//		.MakeGenericMethod(eventType);
-
-			//	var handlerFuncInstance = getHandlderFuncGenericMethod.Invoke(null, null);
-
-			//	genericRegisterHandlerMethod.Invoke(_eventHandlerRegistrar, new object[] { handlerFuncInstance });
-			//}
+			foreach (var registeredEvent in allRegisteredEvents)
+			{
+				InvokeHandler(registeredEvent.EventType, _eventHandlerRegistrar);
+			}
 		}
 
+		/// <inheritdoc />
 		public Task<int> NotifyAsync(WebHookNotificationRequest notificationRequest)
 		{
-			throw new NotImplementedException();
+			// It is just fo the test
+			using (EventLog eventLog = new EventLog("Application"))
+			{
+				eventLog.Source = "Application";
+				eventLog.WriteEntry($"WebHook notification sent on event \"{notificationRequest.EventId}\"{Environment.NewLine} Body:{notificationRequest.RequestParams?.Body}", EventLogEntryType.Information, 101, 1);
+			}
+			return Task.FromResult(1);
 		}
 
+		/// <inheritdoc />
 		public Task<WebHookResponse> VerifyWebHookAsync(WebHook webHook)
 		{
 			throw new NotImplementedException();
 		}
 
-		/// <summary>
-		/// Constructs generic function of type <see cref="Func&lt;T, CancellationToken, Task&gt;>"/> from the generic event type.
-		/// Used in <see cref="SubscribeToAllEvents"/> through Reflection.
-		/// </summary>
-		/// <typeparam name="T">Generic event type.</typeparam>
-		/// <returns>Handler function.</returns>
-		private Func<T, CancellationToken, Task> GetHandlderFunc<T>()
+		private void InvokeHandler(Type eventType, IHandlerRegistrar registrar)
 		{
-			var eventType = typeof(T);
-			var eventId = eventType.Name;
+			var registerExecutorMethod = registrar
+				.GetType()
+				.GetMethods(BindingFlags.Instance | BindingFlags.Public)
+				.Where(mi => mi.Name == "RegisterHandler")
+				.Where(mi => mi.IsGenericMethod)
+				.Where(mi => mi.GetGenericArguments().Length == 1)
+				.Single(mi => mi.GetParameters().Length == 1)
+				.MakeGenericMethod(eventType);
 
-			return (Func<T, CancellationToken, Task>)Delegate.CreateDelegate(typeof(T), new Func<T, CancellationToken, Task>(async (message, token) =>
+			Func<DomainEvent, CancellationToken, Task> del = (x, token) =>
 			{
+				return HandleEvent(x, token);
+			};
+
+			registerExecutorMethod.Invoke(registrar, new object[] { del });
+		}
+
+		private async Task HandleEvent(DomainEvent domainEvent, CancellationToken cancellationToken)
+		{
+			// TODO: Need to handle all this work in BackgroundJob. Handler should just schedule the job, all other handling (webhooks search, notification) should be done later in backround.
+			var eventId = domainEvent.GetType().FullName;
+			var criteria = new WebHookSearchCriteria()
+			{
+				IsActive = true,
+				EventIds = new[] { eventId },
+				Skip = 0,
+				Take = int.MaxValue,
+			};
+			// Make batch handling
+			var webHookdsSearchResult = _webHookSearchService.Search(criteria);
+
+			foreach (var webHook in webHookdsSearchResult.Results)
+			{
+				cancellationToken.ThrowIfCancellationRequested();
 				await NotifyAsync(new WebHookNotificationRequest()
 				{
 					EventId = eventId,
 					RequestParams = new WebHookHttpParams()
 					{
-						Body = JsonConvert.SerializeObject(message),
+						Body = JsonConvert.SerializeObject(domainEvent),
 					}
 				});
-			}).Method);
+			}
 		}
 	}
 }
