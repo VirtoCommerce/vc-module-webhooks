94175deea57346d91720514dec978d4edfe0d784
Andrey Kozlov <yecli.github@gmail.com>
2019-11-18 17:18:45 +0500

    VP-912: WebHookSender
    - Added WebHookWorkItem;
    - Added IWebHookLogger to log send results;
    - Added added abstract WebHookSender that prepares sending data;
    - Added CommonAssemblyInfo, .editorconfig and some other files like License;

--- VirtoCommerce.WebHooksModule.Core/ModuleConstants.cs
+++ VirtoCommerce.WebHooksModule.Core/ModuleConstants.cs
@@ -12,5 +12,13 @@ namespace VirtoCommerce.WebHooksModule.Core
                 public const string ReadFeed = "webhooksFeed:read";
             }
         }
+
+        public static class Log
+        {
+            public static class Templates
+            {
+                public const string InvalidHeader = "Could not add header field '{0}' to the WebHook request for WebHook ID '{1}'.";
+            }
+        }
     }
 }
