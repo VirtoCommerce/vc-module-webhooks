0fce08823e82edc6f8609634aeb64bbc8c2087b7
Andrey Kozlov <yecli.github@gmail.com>
2019-11-19 12:36:18 +0500

    VP-932: RecordTypes to WebHookFeedSearchCriteria

--- VirtoCommerce.WebHooksModule.Core/Models/WebhookFeedSearchCriteria.cs
+++ VirtoCommerce.WebHooksModule.Core/Models/WebhookFeedSearchCriteria.cs
@@ -1,11 +1,12 @@
-﻿using VirtoCommerce.Domain.Commerce.Model.Search;
+using VirtoCommerce.Domain.Commerce.Model.Search;
 
 namespace VirtoCommerce.WebHooksModule.Core.Models
 {
-	public class WebHookFeedSearchCriteria : SearchCriteriaBase
-	{
-		public string[] WebHookIds { get; set; }
-		public string[] EventIds { get; set; }
-		public int[] Statuses { get; set; }
-	}
+    public class WebHookFeedSearchCriteria : SearchCriteriaBase
+    {
+        public string[] WebHookIds { get; set; }
+        public string[] EventIds { get; set; }
+        public int[] Statuses { get; set; }
+        public int[] RecordTypes { get; set; }
+    }
 }
