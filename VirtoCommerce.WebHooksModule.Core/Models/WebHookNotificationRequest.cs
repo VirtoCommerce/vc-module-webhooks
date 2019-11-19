3df1b64f43d6a7f678aaf9e2cfea8bf888dde159
Andrey Kozlov <yecli.github@gmail.com>
2019-11-19 12:15:31 +0500

    VP-913: WebHookSender (#14)
    - Added WebHookWorkItem;
    - Added IWebHookLogger to log send results;
    - Added abstract WebHookSender that prepares sending data;
    - Added CommonAssemblyInfo, .editorconfig and some other files like License;
    - Added RetriableWebHookSender (without retry for now);
    - Added background execution;

--- VirtoCommerce.WebHooksModule.Core/Models/WebHookNotificationRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VirtoCommerce.WebHooksModule.Core.Models
-{
-	public class WebHookNotificationRequest
-	{
-		public string EventId { get; set; }
-		public WebHookHttpParams RequestParams { get; set; }
-	}
-}
