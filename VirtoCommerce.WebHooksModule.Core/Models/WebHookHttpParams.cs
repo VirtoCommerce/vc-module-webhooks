0a07501bd748c13f091080963203d961ab0ef0b6
Andrey Kozlov <yecli.github@gmail.com>
2019-11-19 18:10:36 +0500

    VP-913: WebHookSender
    - Proper Json event data on request body;

--- VirtoCommerce.WebHooksModule.Core/Models/WebHookHttpParams.cs
+++ VirtoCommerce.WebHooksModule.Core/Models/WebHookHttpParams.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace VirtoCommerce.WebHooksModule.Core.Models
 {
-	public class WebHookHttpParams
-	{
-		public Dictionary<string, string> Headers { get; set; } = new Dictionary<string, string>();
-		public string Body { get; set; }
-	}
+    public class WebHookHttpParams
+    {
+        public Dictionary<string, string> Headers { get; set; } = new Dictionary<string, string>();
+        public object Body { get; set; }
+    }
 }
