dd49e5554994f6b1f3ae426ef615b44d6918e7ee
Andrey Kozlov <yecli.github@gmail.com>
2019-11-20 13:44:39 +0500

    VP-912: Event discovery
    - Proper search for DomainEvent descendants;

--- VirtoCommerce.WebHooksModule.Data/Services/RegisteredEventStore.cs
+++ VirtoCommerce.WebHooksModule.Data/Services/RegisteredEventStore.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using VirtoCommerce.Platform.Core.Events;
 using VirtoCommerce.WebHooksModule.Core.Models;
@@ -6,44 +6,44 @@ using VirtoCommerce.WebHooksModule.Core.Services;
 
 namespace VirtoCommerce.WebHooksModule.Data.Services
 {
-	public class RegisteredEventStore : IRegisteredEventStore
-	{
-		private RegisteredEvent[] _registeredEvents;
-		private readonly object _lock = new object();
+    public class RegisteredEventStore : IRegisteredEventStore
+    {
+        private RegisteredEvent[] _registeredEvents;
+        private readonly object _lock = new object();
 
-		public RegisteredEvent[] GetAllEvents()
-		{
-			if (_registeredEvents == null)
-			{
-				lock (_lock)
-				{
-					if (_registeredEvents == null)
-					{
-						_registeredEvents = DiscoverAllDomainEvents();
-					}
-				}
+        public RegisteredEvent[] GetAllEvents()
+        {
+            if (_registeredEvents == null)
+            {
+                lock (_lock)
+                {
+                    if (_registeredEvents == null)
+                    {
+                        _registeredEvents = DiscoverAllDomainEvents();
+                    }
+                }
 
-			}
-			return _registeredEvents;
-		}
+            }
+            return _registeredEvents;
+        }
 
-		private static RegisteredEvent[] DiscoverAllDomainEvents()
-		{
-			var eventInterfaceType = typeof(DomainEvent);
+        private static RegisteredEvent[] DiscoverAllDomainEvents()
+        {
+            var eventBaseType = typeof(DomainEvent);
 
-			var result = AppDomain.CurrentDomain.GetAssemblies()
-				// Maybe there is a way to find platform- and modules- related assemblies
-				.Where(x => !(x.FullName.ToLower().StartsWith("microsoft.") || x.FullName.ToLower().StartsWith("system.")))
-				.SelectMany(x => x.GetTypes())
-				.Where(x => !x.IsAbstract && !x.IsGenericTypeDefinition && eventInterfaceType.IsAssignableFrom(x))
-				.Select(x => new RegisteredEvent()
-				{
-					Id = x.FullName,
-					EventType = x,
-				})
-				.Distinct()
-				.ToArray();
-			return result;
-		}
-	}
+            var result = AppDomain.CurrentDomain.GetAssemblies()
+                // Maybe there is a way to find platform- and modules- related assemblies
+                .Where(x => !(x.FullName.ToLower().StartsWith("microsoft.") || x.FullName.ToLower().StartsWith("system.")))
+                .SelectMany(x => x.GetTypes())
+                .Where(x => !x.IsAbstract && !x.IsGenericTypeDefinition && x.IsSubclassOf(eventBaseType))
+                .Select(x => new RegisteredEvent()
+                {
+                    Id = x.FullName,
+                    EventType = x,
+                })
+                .Distinct()
+                .ToArray();
+            return result;
+        }
+    }
 }
