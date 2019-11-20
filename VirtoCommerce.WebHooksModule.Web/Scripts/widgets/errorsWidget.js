5e497621e8ee6ed86f129de0d3ac222106081a54
Aleksandr Andreev <alivemenone@gmail.com>
2019-11-20 14:22:24 +0300

    add error list blade

--- VirtoCommerce.WebHooksModule.Web/Scripts/widgets/errorsWidget.js
+++ VirtoCommerce.WebHooksModule.Web/Scripts/widgets/errorsWidget.js
@@ -1,7 +1,16 @@
-﻿angular.module('virtoCommerce.webhooksModule')
+angular.module('virtoCommerce.webhooksModule')
     .controller('virtoCommerce.webhooksModule.errorsWebhookController', ['$state', '$scope', 'platformWebApp.bladeNavigationService', function ($state, $scope, bladeNavigationService) {
         var blade = $scope.widget.blade;
         $scope.openBlade = function () {
+            var newBlade = {
+                id: "webhookErrors",
+                webHookId: listItem.id,
+                title: 'webhooks.blades.webhook-detail.title',
+                subtitle: 'webhooks.blades.webhook-detail.subtitle',
+                controller: 'virtoCommerce.webhooksModule.webhookDetailController',
+                template: 'Modules/$(virtoCommerce.webhooksModule)/Scripts/blades/webhook-detail.tpl.html'
+            };
+            bladeNavigationService.showBlade(newBlade, blade);
         
         };
     }]);
