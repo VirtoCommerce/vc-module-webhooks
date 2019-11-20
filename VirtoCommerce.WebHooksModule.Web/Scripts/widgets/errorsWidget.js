dde3173926c3cd40800a84a93ec3ea9ae018e669
Andrey Kozlov <yecli.github@gmail.com>
2019-11-21 00:13:40 +0500

    VP-914: Retry policy
    - Removed redundant methods;
    - Fixed Sonar warnings;

--- VirtoCommerce.WebHooksModule.Web/Scripts/widgets/errorsWidget.js
+++ VirtoCommerce.WebHooksModule.Web/Scripts/widgets/errorsWidget.js
@@ -1,6 +1,6 @@
-﻿angular.module('virtoCommerce.webhooksModule')
+angular.module('virtoCommerce.webhooksModule')
     .controller('virtoCommerce.webhooksModule.errorsWebhookController', ['$state', '$scope', 'platformWebApp.bladeNavigationService', function ($state, $scope, bladeNavigationService) {
-        var blade = $scope.widget.blade;
+        //var blade = $scope.widget.blade;
         $scope.openBlade = function () {
         
         };
