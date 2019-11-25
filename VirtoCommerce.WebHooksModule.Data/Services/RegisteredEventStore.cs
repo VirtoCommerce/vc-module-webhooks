9b4b02007585b5a37b682f06ba4e2fe07415d293
Andrey Kozlov <yecli.github@gmail.com>
2019-11-25 14:39:00 +0500

    VP-1020: Fix TypeLoadException
    - Fixed type load exceptions while discovering events;

--- VirtoCommerce.WebHooksModule.Data/Services/RegisteredEventStore.cs
+++ VirtoCommerce.WebHooksModule.Data/Services/RegisteredEventStore.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Reflection;
 using VirtoCommerce.Platform.Core.Events;
 using VirtoCommerce.WebHooksModule.Core.Models;
 using VirtoCommerce.WebHooksModule.Core.Services;
@@ -34,7 +35,7 @@ namespace VirtoCommerce.WebHooksModule.Data.Services
             var result = AppDomain.CurrentDomain.GetAssemblies()
                 // Maybe there is a way to find platform- and modules- related assemblies
                 .Where(x => !(x.FullName.ToLower().StartsWith("microsoft.") || x.FullName.ToLower().StartsWith("system.")))
-                .SelectMany(x => x.GetTypes())
+                .SelectMany(x => GetTypesSafe(x))
                 .Where(x => !x.IsAbstract && !x.IsGenericTypeDefinition && x.IsSubclassOf(eventBaseType))
                 .Select(x => new RegisteredEvent()
                 {
@@ -43,6 +44,22 @@ namespace VirtoCommerce.WebHooksModule.Data.Services
                 })
                 .Distinct()
                 .ToArray();
+            return result;
+        }
+
+        private static Type[] GetTypesSafe(Assembly assembly)
+        {
+            var result = Array.Empty<Type>();
+
+            try
+            {
+                result = assembly.GetTypes();
+            }
+            catch (Exception ex) when (ex is ReflectionTypeLoadException || ex is TypeLoadException)
+            {
+                // No need to trow as we could have exceptions when loading types
+            }
+
             return result;
         }
     }
