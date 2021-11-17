0b312e623fcb774c38e85bdcc8226358e0bddadc
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2021-11-17 16:11:18 +0200

    Add method to interface

--- src/VirtoCommerce.WebHooksModule.Core/Services/IRegisteredEventStore.cs
+++ src/VirtoCommerce.WebHooksModule.Core/Services/IRegisteredEventStore.cs
@@ -1,17 +1,20 @@
-﻿using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Events;
 using VirtoCommerce.WebHooksModule.Core.Models;
 
 namespace VirtoCommerce.WebHooksModule.Core.Services
 {
-	/// <summary>
-	/// Provides an abstraction for getting all registered events.
-	/// </summary>
-	public interface IRegisteredEventStore
-	{
-		/// <summary>
-		/// Gets all registered <see cref="IEvent"/> instances.
-		/// </summary>
-		/// <returns>All registered <see cref="RegisteredEvent"/> events.</returns>
-		RegisteredEvent[] GetAllEvents();
-	}
+    /// <summary>
+    /// Provides an abstraction for getting all registered events.
+    /// </summary>
+    public interface IRegisteredEventStore
+    {
+        /// <summary>
+        /// Gets all registered <see cref="IEvent"/> instances.
+        /// </summary>
+        /// <returns>All registered <see cref="RegisteredEvent"/> events.</returns>
+        RegisteredEvent[] GetAllEvents();
+
+        //TODO: Add description
+        string[] GetEventObjectProperties(string eventType);
+    }
 }
