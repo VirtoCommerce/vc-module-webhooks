db3bc2d96ff050ed2b78d50f1942fb1f91948692
Basil Kotov <basilkot@gmail.com>
2024-04-18 11:31:03 +0100

    add swagger schema id

--- src/VirtoCommerce.WebHooksModule.Core/Models/RegisteredEvent.cs
+++ src/VirtoCommerce.WebHooksModule.Core/Models/RegisteredEvent.cs
@@ -1,11 +1,13 @@
 using System;
 using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.Platform.Core.Swagger;
 
 namespace VirtoCommerce.WebHooksModule.Core.Models
 {
-	public class RegisteredEvent : Entity
-	{
-		public Type EventType { get; set; }
+    [SwaggerSchemaId("WebHookRegisteredEvent")]
+    public class RegisteredEvent : Entity
+    {
+        public Type EventType { get; set; }
 
         public string DisplayName { get; set; }
     }
