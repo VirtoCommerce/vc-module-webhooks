8bc704344ab9bc58e0163394342e78d9d889611c
Artem Dudarev <artem@virtoworks.com>
2025-11-25 17:13:42 +0200

    Remove IsAllEvents

---
+++ src/VirtoCommerce.WebhooksModule.Data.MySql/Migrations/20251125151241_Webhooks_RemoveIsAllEvents.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.WebhooksModule.Data.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class Webhooks_RemoveIsAllEvents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAllEvents",
+                table: "WebHook");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAllEvents",
+                table: "WebHook",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
